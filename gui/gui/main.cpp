64c6e72417100c8e57ffbee10feac8b2fcedd119
cczy <wyzycao@gmail.com>
2018-08-19 09:15:14 +0800

    添加 Lua 脚本

--- gui/gui/main.cpp
+++ gui/gui/main.cpp
@@ -18,6 +18,14 @@
  动画管理: 借鉴iOS上的Core Animation
  单元测试: google test
  */
+
+extern "C"
+{
+#include "lua.h"
+#include "lauxlib.h"
+#include "lualib.h"
+}
+
 #include <iostream>
 #include <string>
 
@@ -28,6 +36,11 @@
 #include "GuiButton.hpp"
 
 #include "gtest/gtest.h"
+#include "lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
+
+
 
 //using std::string;
 
@@ -56,9 +69,53 @@ void RunGui() {
     GcGui.run();
 }
 
+void configFromLua(lua_State *L) {
+    // int n1 = 1;
+    // int n2 = 2;
+    lua_getglobal(L, "config");
+    // lua_pushstring(L, "aaaa");
+    // 2 是参数数量
+    // 1 是返回值的数量
+    // 0 是 lua 中的错误处理函数，这里不提供所以传 0
+    // 检查 pcall 的返回值以捕捉调用错误
+    if(lua_pcall(L, 0, 3, 0) != 0) {
+        printf("LUA ERROR: %s\n", lua_tostring(L, -1));
+    }
+    const char * t = lua_tostring(L, 0);
+    int w = lua_tonumber(L, 1);
+    int h = lua_tonumber(L, 2);
+    lua_pop(L, 1);
+    // printf("n1: %s\n", n1);
+    // lua_pop(L, 2);
+    // lua_pop(L, 3);
+    printf("t: %s, h:%d w:%d\n", t, w, h);
+}
+
+int drawLine(lua_State *L) {
+    printf("drawLine----\n");
+    return 1;
+}
+
+void runLua() {
+    lua_State *L = luaL_newstate();
+    luaL_openlibs(L);
+    
+    lua_register(L, "drawLine", drawLine);
+    
+    // 载入 lua 文件并执行
+    if(luaL_dofile(L, "/Users/Shared/github/GcUI-cpp/gui/gui/script/config.lua")) {
+        printf("LUA ERROR config: %s\n", lua_tostring(L, -1));
+    }
+    
+    configFromLua(L);
+}
+
 int main(int argc,  char ** argv) {
-    RunTest(argc, argv);
+    runLua();
+//    RunTest(argc, argv);
     RunGui();
+    
+//    lua_close(L);
     return 0;
 }
 
